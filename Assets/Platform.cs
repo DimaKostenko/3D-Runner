ca7f861356d48261a884d43a8bf74dfa54fa5476
Dmitriy Kostenko <dima.kostenko.97@gmail.com>
2020-06-10 22:03:50 +0300

    Added draft mechanic of barriers spawning.

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -7,13 +7,24 @@ public class Platform : MonoBehaviour
     [SerializeField]
     private List<MeshRenderer> meshes; 
     public float platformLenth;
+    [SerializeField]
+    private Transform barriers;
+    [SerializeField]
+    private List<GameObject> barrierBlocks;
+    [SerializeField]
+    private float distanceBetweenBarrierBlocks;
 
     void Start()
     {
+        // рандомный цвет для элементов платформы
         Color platformColor = Random.ColorHSV();
         for(int i = 0; i < meshes.Count; i++){
             meshes[i].material.color = platformColor;
         }
-        //GetComponent<Renderer>().material.color = Random.ColorHSV();
+        // создаем препятствия
+        Vector3 spawnBarrierPoint = new Vector3(0f, 0f, -platformLenth/2 + distanceBetweenBarrierBlocks); //Точка начала платформы
+        int randomIndex = Random.Range(0, barrierBlocks.Count);
+        GameObject barrier = Instantiate(barrierBlocks[randomIndex], barriers);
+        barrier.transform.localPosition = spawnBarrierPoint;
     }
 }
