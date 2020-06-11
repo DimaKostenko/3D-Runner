7ea5acc0cc6de01c6950e459a0cc66224851d30f
Dmitriy Kostenko <dima.kostenko.97@gmail.com>
2020-06-12 02:00:29 +0300

    Added first game flow logic, draft ingame UI.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private float timeFromCoin;
+    [SerializeField]
+    private float scoreFromCoin;
+    public float time;
+    [SerializeField]
+    private Text timerText; 
+    public float score;
+    [SerializeField]
+    private Text scoreText; 
+
+
+    float seconds, minutes, milliseconds;
+    private void Start() {
+        StartCoroutine(StartTimer());
+        //timerText.text = time.ToString();
+    }
+
+    public void IncreaseScore(){
+        score += scoreFromCoin;
+        scoreText.text = score.ToString();
+    }
+
+    public void AddTimeToTimer(){
+        time += timeFromCoin;
+        timerText.text = SetTimerFormat(time);
+    }
+
+    IEnumerator StartTimer()
+    {   
+        while(time > 0f){
+            time -= Time.deltaTime;
+            timerText.text = SetTimerFormat(time);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private string SetTimerFormat(float time){
+        float _time = time;
+        minutes = (int)(_time / 60f);
+        seconds = (int)(_time % 60f);
+        milliseconds = (int)(_time * 100f) % 100;
+        StringBuilder sb = new StringBuilder();
+        sb.Append(minutes.ToString("00"));
+        sb.Append(":");
+        sb.Append(seconds.ToString("00"));
+        sb.Append(":");
+        sb.Append(milliseconds.ToString("00"));
+        return sb.ToString();
+    }
+
+    public void StopTimer(){
+        StopCoroutine(StartTimer());
+    }
+
     public void EndGame(){
         Debug.Log("EndGame");
     }
--- Assets/PlayerCollider.cs
+++ Assets/PlayerCollider.cs
@@ -7,6 +7,8 @@ public class PlayerCollider : MonoBehaviour
     void OnTriggerEnter(Collider col){
         if (col.gameObject.tag == "Coin"){
             GameStorage.Instance.GameManager.CoinCollected();
+            GameStorage.Instance.GameManager.AddTimeToTimer();
+            GameStorage.Instance.GameManager.IncreaseScore();
         }
         if (col.gameObject.tag == "Barrier"){
             col.gameObject.GetComponent<IBarrier>().OnPlayerCollisionEnter();
